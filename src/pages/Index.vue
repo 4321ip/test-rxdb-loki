0cc9d7edfaa505904fd4c27ec6f793becda57a09
igor <4321ip@mail.ru>
2021-12-10 22:35:53 +0300

    find sleep

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,35 +1,74 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-btn
+        icon="clear"
+        label="CLEAR DB"
+        color="red"
+        @click="resetDB"
+    />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import { getRxStorageLoki } from 'rxdb/plugins/lokijs'
-import { createRxDatabase } from 'rxdb'
+import { createRxDatabase, addRxPlugin } from 'rxdb/plugins/core'
+import { RxDBAjvValidatePlugin } from 'rxdb/plugins/ajv-validate'
+import { RxDBLeaderElectionPlugin } from 'rxdb/plugins/leader-election'
+import { RxDBUpdatePlugin } from 'rxdb/plugins/update'
+
+addRxPlugin(RxDBUpdatePlugin)
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode'
+
+addRxPlugin(RxDBDevModePlugin)
+
+addRxPlugin(RxDBLeaderElectionPlugin)
+addRxPlugin(RxDBAjvValidatePlugin)
 const LokiIncrementalIndexedDBAdapter = require('lokijs/src/incremental-indexeddb-adapter')
 
 export default defineComponent({
   name: 'PageIndex',
+  async mounted () {
+    await this.createDB()
+  },
   methods: {
-    async test () {
-      this.rxStorage = getRxStorageLoki({
-        adapter: new LokiIncrementalIndexedDBAdapter()
-        // * Do not set lokiJS persistence options like autoload and autosave,
-        // * RxDB will pick proper defaults based on the given adapter
+    async createDB () {
+      if (!this.db) {
+        this.rxStorage = getRxStorageLoki({
+          adapter: new LokiIncrementalIndexedDBAdapter()
+        })
+        this.db = await createRxDatabase({
+          name: 'db',
+          multiInstance: true,
+          // ignoreDuplicate: true,
+          storage: this.rxStorage
+        })
+      }
+      await this.db.addCollections({
+        // key = collectionName
+        humans: {
+          schema: {
+            type: 'object',
+            version: 0,
+            primaryKey: 'name',
+            properties: {
+              name: {
+                type: 'string'
+              },
+              name2: {
+                type: 'string'
+              }
+            }
+          }
+        }
       })
-      this.db = await createRxDatabase({
-        name: 'kalpadb',
-        storage: this.rxStorage,
-        multiInstance: true // <- multiInstance (optional, default: true)
-        // eventReduce: false // если поставить true - будут теряться события об обновлении (по всей видимости - это баг)<- eventReduce (optional, default: true)
-        // pouchSettings: { revs_limit: 1 }
-      }) // 400 msec
+      console.log('before exec')
+      const docs = await this.db.humans.find().exec()
+      console.log('after exec', docs.length)
+    },
+    async resetDB () {
+      await this.db.humans.remove()
+      await this.createDB()
     }
   }
 })
